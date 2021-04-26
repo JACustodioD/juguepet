2181bbfd3979509631298657a6c76a3299e8ce81
JACustodioD <jacustodiod@hotmail.com>
2021-04-26 13:32:59 -0500

    component products

---
+++ assets/js/product.js
@@ -0,0 +1,25 @@
+app.component('product', {
+    template: /* vue-html */ `
+        <div class="col">
+            <div class="card">
+                <img :src="product.image" class="card-img-top" :alt="product.name">
+                <div class="card-body">
+                    <h5 class="card-title">{{product.name}}</h5>
+                    <p class="card-text">{{product.description}}</p>
+                    <p class="card-text">Precio: $ {{ new Intl.NumberFormat("es-MX").format(product.price) }}</p>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                        <button class="btn btn-store me-md-2" type="button">Agregar <i class="fas fa-cart-plus"></i> </button>
+                    </div>
+                </div>
+            </div>
+         </div>
+
+    `,
+    props:["product"],
+
+    setup(props){
+        return {
+
+        }
+    }
+});
