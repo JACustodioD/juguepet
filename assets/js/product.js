3743ac7e070cda986474a2ffae7057354db9c1da
JACustodioD <jacustodiod@hotmail.com>
2021-04-29 14:21:13 -0500

    search by type pet

--- assets/js/product.js
+++ assets/js/product.js
@@ -7,7 +7,9 @@ app.component('product', {
                     <h5 class="card-title">{{product.name}}</h5>
                     <p class="card-text">{{product.description}}</p>
                     <p class="card-text">Precio: $ {{ new Intl.NumberFormat("es-MX").format(product.price) }}</p>
-                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                </div>
+                <div class="card-footer">
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-end mb-4 ">
                         <button v-if="product.stock >= 1" class="btn btn-store me-md-2" type="button" @click="sendToCart($event)">Agregar <i class="fas fa-cart-plus"></i> </button>
                         <span v-else class="text-warning">Sin productos en el stock</span>    
                     </div>
